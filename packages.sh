6a7d1bc51442b745b47c93a0942158e1491f23bb
toogoodbruh <martialartsapplemma@gmail.com>
2016-09-06 05:24:49 -0400

    changes

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/TestTheme debs
 dpkg-deb -bZgzip projects/CydiaLogoTheme debs
-dpkg-deb -bZgzip projects/Simplified debs
 dpkg-deb -bZgzip projects/SimplifiedRespringLogo debs
 dpkg-deb -bZgzip projects/CydiaIcons debs
+dpkg-deb -bZgzip projects/Simplified debs
