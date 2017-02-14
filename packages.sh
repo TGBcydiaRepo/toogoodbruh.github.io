250091bdad32f031abe6a7dbfd7f829b5669cf1b
toogoodbruh <gnydick@server.toogoodbruh.com>
2017-02-13 21:34:34 -0500

    Changes

--- packages.sh
+++ packages.sh
@@ -4,4 +4,3 @@ dpkg-deb -bZgzip projects/CydiaLogoTheme debs
 dpkg-deb -bZgzip projects/SimplifiedRespringLogo debs
 dpkg-deb -bZgzip projects/CydiaIcons debs
 dpkg-deb -bZgzip projects/Simplified debs
-dpkg-deb -bZgzip projects/Activator debs
