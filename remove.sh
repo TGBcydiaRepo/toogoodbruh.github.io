07ec7899d38102bf32a1ab62ed30463133fdc358
toogoodbruh <martialartsapplemma@gmail.com>
2018-08-20 23:56:31 -0400

    Changes

--- remove.sh
+++ remove.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find ./debs -type f -name '*.deb' -delete
+# find ./debs -type f -name '*.deb' -delete
 find . -name '.DS_Store' -type f -delete
-rm -r packages.bz2
+#rm -r packages.bz2
