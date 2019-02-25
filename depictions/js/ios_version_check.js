ffd00ee2cefd918977755834eb48926303acf5cf
toogoodbruh <martialartsapplemma@gmail.com>
2019-02-25 10:32:17 -0500

    Changes

--- depictions/js/ios_version_check.js
+++ depictions/js/ios_version_check.js
@@ -9,47 +9,17 @@ var VERSION_CHECK_NEEDS_UPGRADE = "Requires at least iOS %s &#x1f615;";
 var VERSION_CHECK_UNCONFIRMED = "Not yet tested on iOS %s &#x1f601;";
 var VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s &#x1f61e;";
 
-/* ADDED
-  [ Detect iOS Version ]
-  supports iOS 2.0 or later
-  call iOSversion() function will get:
-    status: boolean;  -- get iOS version: true, none: false
-    version: int; -- ex 11
-    info: string; -- ex IOS 11.2.6
-*/
-
-function iOSversion() {
-  let d, v;
-  if (/iP(hone|od|ad)/.test(navigator.platform)) {
-    v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
-    d = {
-      status: true,
-      version: parseInt(v[1], 10) ,
-      info: parseInt(v[1], 10)+'.'+parseInt(v[2], 10)+'.'+parseInt(v[3] || 0, 10)
-    };
-  }else{
-    d = {status:false, version: false, info:''}
-  }
-  return d;
-}
-
-let iosVer = iOSversion();
-if (iosVer.status) {
-  alert('iOS : '+iosVer.info+', version : '+iosVer.version);
-  //ex. IOS 11.2.6, version 11
-
-}else{
-  console.log('not iPhone or iPad');
-} //END ADDED
-
-
 function ios_version_check(minIOS,maxIOS,otherIOS,callBack) {
 	"use strict";
 
 
 	function parseVersionString(version) {
 		var bits = version.split(".");
-		return [ bits[0], bits[1] ? bits[1] : 0, bits[2] ? bits[2] : 0 ];
+		return [
+				parseInt(bits[0], 10),
+				parseInt(bits[1] ? bits[1] : 0, 10),
+				parseInt(bits[2] ? bits[2] : 0, 10)
+			   ];
 	}
 
 	function compareVersions(one, two) {
@@ -75,14 +45,16 @@ function ios_version_check(minIOS,maxIOS,otherIOS,callBack) {
 		return 0;
 	}
 
-//navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/
-	//var version = navigator.appVersion.match(/CPU( iPhone)? OS (\d+)_(\d+)(_(\d+))? like/i); original line
-  var version = var version = navigator.appVersion.match(/CPU( iPhone)? OS (\d+)_(\d+)(_(\d+))? like/i));
+	var version = navigator.appVersion.match(/CPU( iPhone)? OS (\d+)_(\d+)(_(\d+))? like/i);
 	if (!version) {
 		return 0;
 	}
 
-	var osVersion = [ version[2], version[3], version[4] ? version[5] : 0 ],
+	var osVersion = [
+						parseInt(version[2], 10),
+						parseInt(version[3], 10),
+						parseInt(version[4] ? version[5] : 0, 10)
+					],
 
 		osString = osVersion[0] + "." + osVersion[1] + (osVersion[2] && osVersion[2] != 0 ? "." + osVersion[2] : ""),
 		minString = minIOS,
